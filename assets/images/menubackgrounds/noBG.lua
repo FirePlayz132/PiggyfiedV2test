27f9c74e34e89899f3641449756734b0458ea0ba
FIre <119700161+FirePlayz132@users.noreply.github.com>
2023-04-13 11:41:24 -0400

    Add files via upload

---
+++ assets/images/menubackgrounds/noBG.lua
@@ -0,0 +1,14 @@
+function onCreate()
+    --sussy death
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'beef');
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'no');
+
+    -- background shit
+    makeLuaSprite('amongus', 'evilBG', 0, 0);
+    setLuaSpriteScrollFactor('amongus', 0.9, 0.9);
+    scaleObject('amongus', 2, 0);
+
+    addLuaSprite('amongus', false);
+    
+    close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
